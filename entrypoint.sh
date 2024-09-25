ee8ac125551ab21dbec93cbd342c4b44de9b0bcb
Krzysztof Taraszka <krzysztof.taraszka@ippnet.pl>
2024-09-25 11:25:50 +0200

    depend disk size on /data size

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-if [[ $(df -PT /var/lib/docker | awk 'NR==2 {print $2}') == virtiofs ]]; then 
+# create disk.img
+if [ ! -f /data/disk.img ]; then
+  total_space=$(df --output=size /data | tail -n 1)
+  total_space_gb=$(echo "($total_space / 1024 / 1024) + 0.5" | bc)
+  total_space_gb_int=$(printf "%.0f" "$total_space_gb")
   apk add e2fsprogs
-  truncate -s 3G /data/disk.img
+  truncate -s "$((total_space_gb_int - 1))G" /data/disk.img
   mkfs.ext4 /data/disk.img
-  mount /data/disk.img /var/lib/docker; 
 fi
+
+# mount disk.img
+mount /data/disk.img /var/lib/docker;
 
 dockerd-entrypoint.sh
